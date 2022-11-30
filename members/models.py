7e91a7cb7844495b8dd155adac96bcbf88714c48
jasmith93 <jasmith93@live.co.uk>
2022-11-30 00:12:53 +0000

    Initial members model

--- members/models.py
+++ members/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    date_of_birth = models.DateTimeField()
+    is_member = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
